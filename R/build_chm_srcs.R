90b239dbfa64390a70b65a61fb36ef25ae72aab7
Hugh Graham <hugh.a.graham@gmail.com>
2024-04-25 11:30:15 +0100

    patch bug where large search areas were missing tiles.

--- R/build_chm_srcs.R
+++ R/build_chm_srcs.R
@@ -12,6 +12,21 @@ build_chm_srcs <- function(target) {
 
   qk <- unique(kuam_cent_qk$quadkey)
 
+  if (length(qk) > 3) {
+    # this catches when the target area intersects more than 3 tiles, in this
+    # case we can build a quadkey grid with thes corner coords and extract all
+    # intersecting tiles.
+    box <- get_ext(target) |>
+      sf::st_bbox() |>
+      sf::st_as_sfc() |>
+      sf::st_set_crs(get_proj(target)) |>
+      sf::st_transform(4326) |>
+      sf::st_bbox()
+
+    qkg <- quadkeyr::create_qk_grid(box[1], box[3], box[2], box[4], zoom = 9)
+    qk <- qkg$data$quadkey
+  }
+
   paste("/vsis3",
     "dataforgood-fb-data", "forests/v1/alsgedi_global_v6_float/chm",
     paste0(qk, ".tif"),
